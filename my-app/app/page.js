5164c66334956fb32227d062a6a4cca0960be36d
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2025-08-25 23:38:17 +0000

    Terceiro commit

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -6,9 +6,9 @@ import { Profile } from "@/componentes/profile";
 export default function Home() {
   return (
     <div>
-      <h1>Sejam bem-vendos! Me chamo Layza Nicolle. Atualmente estudo Sistemas para Internet na Universidade Católica de Pernambuco.
-        Também sou desenvolvedora back-end na Accenture! Procuro me aprofundar ainda mais na área e tirar algumas certificações.
-      </h1>
+      <h1>Site criado para disciplina de Front-end. Sejam bem-vindos!</h1>
+      <h2>Me chamo Layza Nicolle. Atualmente estudo Sistemas para Internet na Universidade Católica de Pernambuco.
+        Também sou desenvolvedora back-end na Accenture! Procuro me aprofundar ainda mais na área e tirar algumas certificações.</h2>
       <Profile />
     </div>
   );
